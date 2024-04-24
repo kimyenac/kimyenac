f92f0394c03c211ddb9f352e6535548578b69cb7
kimyenac <yena.c.2005@gmail.com>
2024-04-25 02:54:11 +0900

    feat: body > background color 제어

--- src/component/BodyContainer.tsx
+++ src/component/BodyContainer.tsx
@@ -1,12 +1,23 @@
 "use client";
 
 import styled from "@emotion/styled";
+import { useEffect } from "react";
+import useThemeMode from "@/hook/useThemeMode";
+import { darkTheme, lightTheme } from "@/constant/theme";
 
 const BodyContainer = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { isDarkMode } = useThemeMode();
+
+  useEffect(() => {
+    document.body.style.background = isDarkMode
+      ? darkTheme.color.defaultBackground
+      : lightTheme.color.defaultBackground;
+  }, [isDarkMode]);
+
   return <Wrap>{children}</Wrap>;
 };
 
